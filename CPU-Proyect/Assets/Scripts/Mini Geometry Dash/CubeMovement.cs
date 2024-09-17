315e8bdb0f5aa0ed5363e8efe7cec1a6267d3424
Ilannda10 <ilannunezdelarco@gmail.com>
2024-09-17 00:35:54 -0500

    Mini GD Salto Perfecto
    I wanna i Hate

---
+++ CPU-Proyect/Assets/Scripts/Mini Geometry Dash/CubeMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float MagnitudDeSalto = 10f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform feetpos;
+    [SerializeField] private float RotationSpeed = 0.5f;
+    [SerializeField] private float DistanciaSuelo = 0.009f;
+
+    [SerializeField] public bool isGrounded = false;
+    private bool isJumping = false;
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetpos.position, DistanciaSuelo, groundLayer);
+        if (isGrounded && Input.GetButton("Jump"))
+        {
+            isJumping = true;
+            rb.velocity = Vector2.up * MagnitudDeSalto;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+    }
+}
+
---
+++ CPU-Proyect/Assets/Scripts/Mini Geometry Dash/SpriteRotation.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    private GameObject Player; // Referencia al componente CubeMovement
+    public CubeMovement cubeMovement;
+    [SerializeField] private float RotationSpeed = 0.82f;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        cubeMovement = Player.GetComponent<CubeMovement>();
+    }
+
+    void Update()
+    {
+        // Comprueba si isGrounded es falso en CubeMovement
+        if (!cubeMovement.isGrounded)   
+        {
+            print("black abbby yoda");
+            transform.Rotate(0, 0, -RotationSpeed); // Rota si no está en el suelo
+        }
+
+        // Detecta cuando el estado cambia de no estar en el suelo a estar en el suelo
+        if (cubeMovement.isGrounded)
+        {
+            AdjustRotationToNearest90(); // Ajusta la rotación al ángulo más cercano
+        }
+
+    }
+
+    private void AdjustRotationToNearest90()
+    {
+
+        float currentRotationZ = transform.eulerAngles.z;
+
+        float nearest90 = Mathf.Round(currentRotationZ / 90f) * 90f;
+
+        transform.rotation = Quaternion.Euler(0, 0, nearest90);
+    }
+}
+
